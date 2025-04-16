1937f6b97e5bf05608b149d411950566e5db3539
Oriol Ologaray <oologaray13@gmail.com>
2025-04-16 21:01:24 +0200

    Add logger and improve booking logic

--- models/booking.py
+++ models/booking.py
@@ -10,5 +10,10 @@ class Booking(BaseModel):
     end_date: date = Field(alias='endDate')
     booker_id: str = Field(alias='bookerId')
 
-    def is_active_on(self, rental_date: date) -> bool:
+    def is_active_on(self, rental_date: date, end_rental_date: date = None) -> bool:
+        # If end_rental_date is passed as a parameter, the function will check if there is overlap between ranges.
+        if end_rental_date:
+            return max(self.start_date, rental_date) <= min(self.end_date, end_rental_date)
+
+        # Else check that the booking is not active for that single day.
         return self.start_date <= rental_date <= self.end_date
--- routes/cars_route.py
+++ routes/cars_route.py
@@ -4,17 +4,22 @@ from fastapi import APIRouter
 
 from crud.crud import get_all_cars, get_all_bookings
 from models.car import Car
+from utils.logger import logger
 
 router = APIRouter()
 
 
 @router.get('/cars/')
 def get_cars() -> list[Car]:
+    logger.info('Attempting to get all cars.')
+
     return get_all_cars()
 
 
 @router.get('/cars/available')
 def get_available_cars(rental_date: date = date.today()) -> list[Car]:
+    logger.info(f'Attempting to get all available cars for date {rental_date}.')
+
     available_car_ids = [booking.car_id for booking in get_all_bookings() if booking.is_active_on(rental_date)]
 
     return [car for car in get_all_cars() if car.id in available_car_ids]
